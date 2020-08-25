8d6db1cb53e1c605bec51727f6595cd37f076328
Janis Goldzycher <janisgoldzycher@mailbox.org>
2020-08-25 17:46:45 +0200

    add bert encoder

---
+++ bert.py
@@ -0,0 +1,44 @@
+# This file is adapted from from May et al. 2019: "On Measuring Bias in Sentence Encoders"
+# File: https://github.com/W4ngatang/sent-bias/blob/master/sentbias/encoders/bert.py
+
+
+import torch
+import pytorch_pretrained_bert as bert
+
+
+MODELS = {
+    'bert': [
+        'bert-base-german-cased',
+        'bert-base-german-dbmdz-cased',
+        'bert-base-german-dbmdz-uncased',
+        'distilbert-base-german-cased'
+    ],
+    'xlm': [
+        'xlm-mlm-ende-1024',
+        'xlm-clm-ende-1024'
+    ]
+}
+
+
+def load_model(version='bert-large-uncased'):
+    ''' Load BERT model and corresponding tokenizer '''
+    tokenizer = bert.BertTokenizer.from_pretrained(version)
+    model = bert.BertModel.from_pretrained(version)
+    model.eval()
+    return model, tokenizer
+
+
+def encode(model, tokenizer, texts):
+    ''' Use tokenizer and model to encode texts '''
+    encs = {}
+    for text in texts:
+        tokenized = tokenizer.tokenize(text)
+        indexed = tokenizer.convert_tokens_to_ids(tokenized)
+        segment_idxs = [0] * len(tokenized)
+        tokens_tensor = torch.tensor([indexed])
+        segments_tensor = torch.tensor([segment_idxs])
+        enc, _ = model(tokens_tensor, segments_tensor, output_all_encoded_layers=False)
+
+        enc = enc[:, 0, :]  # extract the last rep of the first input
+        encs[text] = enc.detach().view(-1).numpy()
+    return encs
